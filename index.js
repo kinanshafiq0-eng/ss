1c541cbb342c1afd71ceb59a4b7b50a10b89848e
kinanshafiq0-eng <kinanshafiq0@gmail.com>
2026-07-20 17:52:18 +0300

    Update index.js

--- index.js
+++ index.js
@@ -179,10 +179,8 @@ const client = new Client({
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.MessageContent,
-    GatewayIntentBits.GuildMembers,
     GatewayIntentBits.GuildVoiceStates,
     GatewayIntentBits.GuildMessageReactions,
-    GatewayIntentBits.GuildPresences,
   ],
 });
 
